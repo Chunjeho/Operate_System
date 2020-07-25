b5998da4892c30217734b74e3405fb3c3941a456
Chunjeho <jejehoho3317@gmail.com>
2020-07-25 21:31:02 +0900

    Update SnakeAndLadder.py

--- SnakeAndLadder.py
+++ SnakeAndLadder.py
@@ -1,11 +1,12 @@
 _path = [[1,38],[1,2],[1,2,3],[1,2,3,4,14],[1,2,3,4,5],[1,2,3,4,5,6]]
 _bowl = [] #kind of buffer memory
-_final = [] #storage for some final paths
+_final = [] #storage for some paths or final path(shortest)
 
 _ladder = {1:38, 4:14, 9:31, 21:42, 28:84, 51:67, 72:91, 80:99} # {from:to}
 _snake = {17:7, 54:34, 62:19, 64:60, 87:36, 93:73, 95:75, 98:79} # {from:to}
-finish = 0
+finish = 0 #Count, if you excess 100 or use for something to count
 
+#make some paths
 while(finish < 6):
     for arr in _path.copy():
         for i in range(1, 7):
@@ -29,10 +30,12 @@ while(finish < 6):
         if(finish == 6):
             break
 
+ #Initiate variables
 _final = min(_final, key=len)
 _bowl.clear()
-_finish = 1
+_finish = 0
 
+#Count to calculate the shortest path
 for k in range(len(_final)-1):
     if(_final[k]+1 != _final[k+1]):
         _bowl.append(_finish)
@@ -44,17 +47,18 @@ _bowl.append(_finish)
 
 _final.clear()
 
+# Show a dice pips to win this game
 for ele in _bowl:
-    if(ele == 0):
+    if(ele == 0): #If you meet ladder in 1 (= dice pip is euqal to 1), in here {1:38} (from 1 to 38)
         _final.append(1)
     else:
-        a = int(ele/6)
-        b = ele%6
+        a = int(ele/6) #How many 6 are needed?
+        b = ele%6 #pips which are lower than 6
         for l in range(a):
             _final.append(6)
         _final.append(b)
 
-print(_final)
+print(_final) #print dice pips
 print(len(_final), "times"))
 
 
